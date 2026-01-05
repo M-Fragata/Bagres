e6675e1813b6c48649f9f0cf4072535bf09d7233
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-01-05 16:48:17 -0300

    security

--- backend/src/mongoDB/DataBase.ts
+++ backend/src/mongoDB/DataBase.ts
@@ -1,11 +1,17 @@
-import mongoose from "mongoose";
+import mongoose from "mongoose"
 
 export const DataBase =  ( async () => {
-    const MONGO_URL = process.env.MONGO_URL || "mongodb+srv://Fragata:m4th3us1@cluster0.bdixruu.mongodb.net/?appName=Cluster0";
+    const MONGO_URL = process.env.MONGO_URL
+
+    if(!MONGO_URL){
+        console.log("Erro ao conectar ao MongoDB: URL")
+        return
+    }
+
     try {
-        await mongoose.connect(MONGO_URL);
-        console.log("Conectado ao MongoDB");
+        await mongoose.connect(MONGO_URL)
+        console.log("Conectado ao MongoDB")
     } catch (error) {
-        console.log("Erro ao conectar ao MongoDB:", error);
+        console.log("Erro ao conectar ao MongoDB:", error)
     }
 })
