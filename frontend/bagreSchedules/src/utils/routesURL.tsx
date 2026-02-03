16b826fbc287a831b21b39ee8b8622a31a920e91
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-02-03 20:21:52 -0300

    alterando as url para o deploy

--- frontend/bagreSchedules/src/utils/routesURL.tsx
+++ frontend/bagreSchedules/src/utils/routesURL.tsx
@@ -4,9 +4,12 @@ type RoutesURLProps = {
     API_SESSION: string;
 }
 
-export const RoutesURL: RoutesURLProps = 
-{
-    "API_LOGIN": "http://localhost:3333/login",
-    "API_SCHEDULES": "http://localhost:3333/schedules",
-    "API_SESSION": "http://localhost:3333/session"
+// O Vite vai tentar ler a variável do arquivo .env. 
+// Se não encontrar (no seu PC), ele usa o localhost como "plano B".
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3333";
+
+export const RoutesURL: RoutesURLProps = {
+    "API_LOGIN": `${BASE_URL}/login`,
+    "API_SCHEDULES": `${BASE_URL}/schedules`,
+    "API_SESSION": `${BASE_URL}/session`
 }
