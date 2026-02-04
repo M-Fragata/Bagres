86a7966047145895ba6d1e60f6c10c9c61b3ff5b
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-02-03 21:37:23 -0300

    Update layout.tsx

--- frontend/bagreSchedules/src/components/layout.tsx
+++ frontend/bagreSchedules/src/components/layout.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom"
-import { useState, useEffect } from "react"
+import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 
 export function LayoutPage() {
