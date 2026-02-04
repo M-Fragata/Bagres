32a81843af7ab1d641be2e87d151687f8f29a176
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-02-03 21:11:01 -0300

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -18,7 +18,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // 3. Responde o "Preflight" (OPTIONS) de forma ultra rápida
-app.options("*", cors(corsOptions));
+app.options("(.*)", cors(corsOptions));
 
 app.use(express.json())
 
