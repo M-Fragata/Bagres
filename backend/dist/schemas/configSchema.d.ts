5110810aed2752b5a864d752c99b19f344244833
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-03-05 21:20:10 -0300

    Alterando configSchema para dias e horarios ser aceito em formatado json

--- backend/dist/schemas/configSchema.d.ts
+++ backend/dist/schemas/configSchema.d.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 export declare const configSchema: z.ZodObject<{
-    dias: z.ZodArray<z.ZodString>;
-    horarios: z.ZodArray<z.ZodString>;
+    dias: z.ZodRecord<z.ZodString, z.ZodBoolean>;
+    horarios: z.ZodRecord<z.ZodString, z.ZodArray<z.ZodString>>;
 }, z.core.$strip>;
 //# sourceMappingURL=configSchema.d.ts.map
--- backend/dist/schemas/configSchema.js
+++ backend/dist/schemas/configSchema.js
@@ -1,6 +1,6 @@
 import { z } from "zod";
 export const configSchema = z.object({
-    dias: z.array(z.string()),
-    horarios: z.array(z.string())
+    dias: z.record(z.string(), z.boolean()),
+    horarios: z.record(z.string(), z.array(z.string()))
 });
 //# sourceMappingURL=configSchema.js.map
