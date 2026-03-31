86ba4192cc9493e024d94120b9aabff7ddbacfcd
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-03-31 18:34:08 -0300

    implementando a funcionalidade de botão desativado para quando o usuário agendar o horário

--- frontend/bagreSchedules/src/components/Button.tsx
+++ frontend/bagreSchedules/src/components/Button.tsx
@@ -1,12 +1,13 @@
 type Props = React.ComponentProps<"button"> &{
     icon?: string;
     title?: string;
+    disabled?: boolean;
 }
 
-export function Button({title, icon, type = "submit", ...rest }: Props) {
+export function Button({title, icon, disabled = false, type = "submit", ...rest }: Props) {
     return (
-        <button {...rest} type={type}
-            className="bg-bagre-primaria text-white px-4 py-2 mt-4 mb-2 rounded-2xl cursor-pointer hover:opacity-95 transition-all duration-200 "
+        <button {...rest} type={type} disabled={disabled}
+            className={`bg-bagre-primaria text-white px-4 py-2 mt-4 mb-2 rounded-2xl cursor-pointer hover:opacity-95 transition-all duration-200 ${disabled ? 'bg-bagre-secundaria cursor-wait' : ''}`}
         >
             {icon && <img src={icon} alt="Icon" className="mr-2 inline-block w-7 p-0 m-0" />}
             {title}
