2e67b3c54903af112fca1ae3c7fb22b4f9058e5b
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-03-31 20:44:43 -0300

    Update ScheduleHours.tsx

--- frontend/bagreSchedules/src/components/ScheduleHours.tsx
+++ frontend/bagreSchedules/src/components/ScheduleHours.tsx
@@ -15,11 +15,11 @@ export function ScheduleHours({ title, initial, final, selected, onSelect, avail
         <div className="flex gap-2">
             <div className="flex flex-col gap-1">
                 <p className="text-center">{title}</p>
-                {availabeHours && availabeHours.map((hour) => {
+                {availabeHours && availabeHours.slice().sort().map((hour) => {
 
                     const hourNumber = Number(hour.replace(":", ""))
                     //08:00 -> 0800
-                    
+
                     if (initial <= hourNumber && hourNumber <= final) {
                         return (<HourButton
                             key={hour}
