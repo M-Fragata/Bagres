77c5d658ccba3147de442ddf299cda85e257addc
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-04-02 18:27:55 -0300

    Pequeno video de um astronauta com um peixe fisgado

--- frontend/bagreSchedules/src/pages/notFound.tsx
+++ frontend/bagreSchedules/src/pages/notFound.tsx
@@ -1,8 +1,30 @@
+import fishAnimation from '../assets/animation/fishCatched.webm'
+import { useNavigate } from 'react-router-dom'
+
 export function NotFoundPage() {
+
+    const navigate = useNavigate()
+
     return (
-        <div>
-            <h1>404 - Page Not Found</h1>
-            <p>The page you are looking for does not exist.</p>
+        <main className='w-full h-dvh flex flex-col items-center justify-center gap-10 bg-bagre-primaria'>
+        <h1 className='text-bagre-terciaria'>Oops! Parece que você foi fisgado!</h1>
+        <div className="w-48 h-48 mx-auto">
+            <video 
+            autoPlay
+            loop 
+            muted 
+            playsInline
+            className="w-full h-full object-cover rounded-2xl">
+                <source src={fishAnimation} type="video/webm" />
+                Your browser does not support the video tag.
+            </video>
         </div>
+        <input 
+        className='text-bagre-terciaria border rounded-2xl py-2 px-6 cursor-pointer hover:bg-bagre-secundaria shadow-2xs transition-all duration-200'
+        type="button" 
+        value="Retorne ao mar" 
+        onClick={() => navigate('/')}
+        />
+        </main>
     )
 }
