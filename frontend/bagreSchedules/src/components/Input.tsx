72de053ba4fc92ed61de04c3050319842ac0357c
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-04-02 13:24:10 -0300

    ajuste no componente de input para uso dos novos icons

--- frontend/bagreSchedules/src/components/Input.tsx
+++ frontend/bagreSchedules/src/components/Input.tsx
@@ -1,17 +1,32 @@
+import { useId } from "react"
+
 type Props = React.ComponentProps<"input"> & {
     legend?: string
     legendClassName?: string
     inputClassName?: string
+    logo?: string
 }
 
-export function Input({legend, type = "text", legendClassName, inputClassName, ...rest }: Props) {
+export function Input({ legend, type = "text", legendClassName, inputClassName, logo, ...rest }: Props) {
+
+    const inputID = useId()
+
     return (
         <fieldset>
-            <legend className={` ${legendClassName}`}>{legend}</legend>
-            <input {...rest}
-                type={type}
-                className={`border border-gray-300 rounded-2xl px-4 py-2 outline-none w-full focus:border-bagre-primaria focus-within:bg-bagre-primaria/50 ${inputClassName}`}
-            />
+
+            <div className="flex">
+                {logo && (
+                    <label htmlFor={inputID} className={`border border-bagre-primaria rounded-l-xl p-2 align-bottom cursor-pointer bg-white hover:bg-bagre-primaria/50 ${legendClassName}`}>
+                        <img src={logo} alt="Logo" className="text-white"/>
+                    </label>
+                )}
+
+                <input {...rest}
+                    type={type}
+                    id={inputID}
+                    className={`border border-bagre-primaria px-4 py-2 outline-none w-full focus-within:bg-bagre-primaria/50 ${inputClassName} ${logo ? 'rounded-r-2xl' : 'rounded-2xl'}`}
+                />
+            </div>
         </fieldset>
 
     )
