22061ccbb3b1961bb4194e166e5863215e924347
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-04-07 18:53:05 -0300

    utilizando o useEffect para verificar a validade do token do usuário, caso esteja vencido redireciona para a tela de login

--- frontend/bagreSchedules/src/App.tsx
+++ frontend/bagreSchedules/src/App.tsx
@@ -1,6 +1,42 @@
 import { Routes } from "./routes/index.tsx"
+import { useEffect } from "react"
+import { jwtDecode } from "jwt-decode";
+import { token } from "./utils/routesURL.tsx"
 
 export function App() {
+
+  //useEffect para verificar se o token do usuário está vencido.
+  useEffect(() => {
+
+    if (token) {
+      try {
+
+        const decoded: any = jwtDecode(token)
+        const agora = Math.floor(Date.now() / 1000);
+
+        console.log(token, agora)
+        console.log(decoded.exp)
+
+        if (decoded.exp < agora) {
+          //Token venceu!
+          localStorage.removeItem("@bagres:token");
+          localStorage.removeItem("@bagres:user");
+          localStorage.removeItem("@bagres:userName");
+
+          alert("Sua sessão expirou. Por favor, faça login novamente.");
+
+          window.location.href = "/";
+        }
+
+      } catch (error) {
+
+        localStorage.clear();
+        window.location.href = "/";
+
+      }
+    }
+
+  }, [])
 
   return (
     <Routes />
